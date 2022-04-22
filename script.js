7330139d8d4409a2c3357648ec375f30af25b6de
Seagull <103934114+ItsTheWhale@users.noreply.github.com>
2022-04-22 16:11:33 +0800

    Small patch

--- script.js
+++ script.js
@@ -3,33 +3,25 @@ let clicks = 0;
 let startTime;
 let runTime = 5000;
 let clickStatus = document.getElementById("clickStatus");
+let dynamicCPS = false;
 const registerClick = () => {
-    if(Date.now() - startTime >= runTime) {
-        endClicker();
-        return;
-    };
     clicks++;
-    clickStatus.innerHTML = "Time Elapsed: " + (Date.now() - startTime) + "ms<br/>" + "Clicks: " + clicks;
+    clickStatus.innerHTML = `Time Elapsed: ${Date.now() - startTime}ms<br/>Clicks: ${clicks + 1}`;
 };
-function initClicker() {;
+function initClicker() {
     startTime = Date.now();
-    clickBox.addEventListener("click", registerClick);
-    clickStatus.innerHTML = "Time Elapsed: 0ms<br/>Clicks: 0";
-};
-function endClicker() {
-    clickBox.removeEventListener("click", registerClick)
-    console.log("Click test ended!");
-    alert("Your CPS is: " + clicks / (runTime / 1000))
-    clicks = 0;
-    clickStatus.innerHTML = "Click anywhere inside to start!";
-    clickBox.addEventListener("click", initListener);
-};
-const initListener = () => {
-    initClicker();
+    setTimeout(() => {
+        clickBox.removeEventListener("click", registerClick)
+        console.log("Click test ended!");
+        alert("Your CPS is: " + clicks / (runTime / 1000))
+        clicks = 0;
+        clickStatus.innerHTML = "Click anywhere inside to start!";
+        clickBox.addEventListener("click", initClicker);
+    }, runTime);
     console.log("Click test started!");
-    clickBox.removeEventListener("click", initListener);
+    clickBox.removeEventListener("click", initClicker);
+    clickBox.addEventListener("click", registerClick);
+    clickStatus.innerHTML = "Time Elapsed: 0ms<br/>Clicks: 1";
+    if(dynamicCPS) clickStatus.innerHTML = "Time Elapsed: 0ms<br/>Clicks: "
 };
-const editRunTime = () => {
-    runTime = window.prompt("Enter new test time in ms:");
-};
-clickBox.addEventListener("click", initListener);
+clickBox.addEventListener("click", initClicker);
